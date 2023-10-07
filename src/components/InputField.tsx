996a8444e5e3a3a168fa8ef29daf09ad954c39dd
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-10-07 22:25:11 +0530

    build input component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
+  // const InputField:React.FC<Props> = ({todo, setTodo}) => {   //above line can also be written as this but this is not used much and not advised also.
+  return (
+    <form className="border border-gray-600 rounded-full w-80 flex justify-evenly items-center h-11">
+      <input
+        type="input"
+        placeholder="Add Task"
+        className="outline-none w-64"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="w-9 h-9 rounded-full bg-[#138086] -mr-2 text-white hover:scale-95"
+      >
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
