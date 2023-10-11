42a114e787928bc712e60e324a2a453a13142f26
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-10-11 22:56:01 +0530

    build the edit task fucntionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { ToDo } from "../model";
 import { AiFillEdit, AiFillDelete, AiOutlineCheck } from "react-icons/ai";
 import ToDoList from "./ToDoList";
+import { useState } from "react";
 
 interface Props {
   todo: ToDo;
@@ -10,6 +11,8 @@ interface Props {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
   const handleDone = (id: number) => {
     setTodos(
@@ -25,13 +28,48 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         todo.id !== id 
       )
     ); 
-  }   
+  } 
+  
+  const handleEdit = (e:React.FormEvent,id:number)=> {
+    e.preventDefault();
+
+    setTodos(todos.map((todo)=> (
+      todo.id===id?{...todo, todo:editTodo}:todo
+    )));
+    setEdit(false);
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(()=> {
+    inputRef.current?.focus();
+  },[edit])
 
   return (
-    <form className="w-96 h-14 overflow-clip bg-[#CD7672] border rounded-3xl flex items-center justify-between px-4 text-lg text-white font-medium">
-      {todo.isDone ? <s>{todo.todo}</s> : <span>{todo.todo}</span>}
+    <form className="w-96 h-14 overflow-clip bg-[#CD7672] border rounded-3xl flex items-center justify-between px-4 text-lg text-white font-medium"
+    onSubmit={(e)=>{handleEdit(e, todo.id)}}
+    >
+      {
+        edit ? (
+          <input value={editTodo} ref={inputRef} onChange={(e)=>setEditTodo(e.target.value)}
+          className="text-black outline-none"
+          />
+        )
+        :
+        (
+          todo.isDone ? <s>{todo.todo}</s> : <span>{todo.todo}</span>
+        )
+      }
+
+      {/* {todo.isDone ? <s>{todo.todo}</s> : <span>{todo.todo}</span>} */}
       <div className="flex gap-x-2 [&>*]:cursor-pointer">
-        <span>
+        <span
+        onClick={() =>
+          {if(!edit && !todo.isDone){
+            setEdit(!edit)
+          }}
+        }
+        >
           <AiFillEdit />
         </span>
         <span
