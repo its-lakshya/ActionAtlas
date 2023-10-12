f7fd5844d009493af7d7095d57d25bf459f32deb
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-10-12 23:27:43 +0530

    formated the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,45 @@
-import React, { FormEvent } from 'react';
-import './App.css';
-import InputField from './components/InputField';
-import { useState } from 'react';
-import { ToDo } from './model';
-import List from './components/ToDoList';
-import ToDoList from './components/ToDoList';
+import React, { FormEvent } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { useState } from "react";
+import { ToDo } from "./model";
+import List from "./components/ToDoList";
+import ToDoList from "./components/ToDoList";
 
-const App:React.FC = () =>  {
-
-  const [todo, setTodo] = useState<string>("")
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<ToDo[]>([]);
 
-  const handleAdd = (e:React.FormEvent) => {
+  const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if(todo){
-      setTodos([...todos, {id:Date.now(), todo:todo, isDone:false}])
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
       setTodo("");
     }
-  }
+  };
 
   return (
     <div className="flex flex-col w-full h-screen items-center my-10 mx-10 max-sm:my-5 max-sm:mx-5 gap-y-10">
-      <div className='font-semibold text-3xl'>ActionAtlas</div>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <div className="font-semibold text-3xl">ActionAtlas</div>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       {/* <div className='flex flex-col gap-y-2'>
       {todos.map((todo) => {
         return(
         <List key={todo.id} id={todo.id} todo={todo.todo} isDone={todo.isDone}/>        
       )})}
       </div> */}
-      <ToDoList todos={todos} setTodos={setTodos}/>
+      <ToDoList todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
 
-
 // // let id:any; //this is not recommended
 // // let id:unknown; //instead of any we can use unknown
-// let name:string; 
-// // let name:string | number; // this is union operator now out name can have both string and number 
+// let name:string;
+// // let name:string | number; // this is union operator now out name can have both string and number
 
 // let age:number;
 // let isStudent:boolean;
@@ -52,36 +50,35 @@ export default App;
 // let handleClick :(name:string, age:number) => void; //this will return unknown;
 // let handleClick2 :(name:string, age:number) => never; //this will return nothing;
 
-
 // //type and interface are called alias
 // type Person={
-//   name:string, 
+//   name:string,
 //   age?:number, //this ? made the age optional
 // }
 
 // type Animal = Person & {   //extending the type animal with the type person
-//   color:string, 
+//   color:string,
 //   breed:string,
 // }
 
 // let person:Person
 
 // interface Person2{
-//   name:string, 
-//   age?:number, 
+//   name:string,
+//   age?:number,
 // }
 
 // interface Animal2 extends Person2{   //extending the interface Animal2 with the interface Person2
-//   color:string, 
-//   breed:string, 
+//   color:string,
+//   breed:string,
 // }
 
 // interface Animal3 extends Person{   //extending the interface Animal2 with the type Person2
-//   color:string, 
-//   breed:string, 
+//   color:string,
+//   breed:string,
 // }
 
 // type Animal4= Person2 &{   // extending the type Animal4 with the interface Person2
-//   color:string, 
+//   color:string,
 //   bree:string,
 // }
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -19,62 +19,60 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
       todos.map((todo) =>
         todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
       )
-    ); 
+    );
   };
-  
+
   const handleDelete = (id: number) => {
-    setTodos(
-      todos.filter((todo) =>
-        todo.id !== id 
-      )
-    ); 
-  } 
-  
-  const handleEdit = (e:React.FormEvent,id:number)=> {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
     e.preventDefault();
 
-    setTodos(todos.map((todo)=> (
-      todo.id===id?{...todo, todo:editTodo}:todo
-    )));
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
     setEdit(false);
-  }
+  };
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(()=> {
+  useEffect(() => {
     inputRef.current?.focus();
-  },[edit])
+  }, [edit]);
 
   return (
-    <form className="w-96 h-14 overflow-clip bg-[#CD7672] border rounded-3xl flex items-center justify-between px-4 text-lg text-white font-medium"
-    onSubmit={(e)=>{handleEdit(e, todo.id)}}
+    <form
+      className="w-96 h-14 overflow-clip bg-[#CD7672] border rounded-3xl flex items-center justify-between px-4 text-lg text-white font-medium"
+      onSubmit={(e) => {
+        handleEdit(e, todo.id);
+      }}
     >
-      {
-        edit ? (
-          <input value={editTodo} ref={inputRef} onChange={(e)=>setEditTodo(e.target.value)}
+      {edit ? (
+        <input
+          value={editTodo}
+          ref={inputRef}
+          onChange={(e) => setEditTodo(e.target.value)}
           className="text-black outline-none"
-          />
-        )
-        :
-        (
-          todo.isDone ? <s>{todo.todo}</s> : <span>{todo.todo}</span>
-        )
-      }
+        />
+      ) : todo.isDone ? (
+        <s>{todo.todo}</s>
+      ) : (
+        <span>{todo.todo}</span>
+      )}
 
       {/* {todo.isDone ? <s>{todo.todo}</s> : <span>{todo.todo}</span>} */}
       <div className="flex gap-x-2 [&>*]:cursor-pointer">
         <span
-        onClick={() =>
-          {if(!edit && !todo.isDone){
-            setEdit(!edit)
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
           }}
-        }
         >
           <AiFillEdit />
         </span>
-        <span
-        onClick={()=> handleDelete(todo.id)}
-        >
+        <span onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
         <span onClick={() => handleDone(todo.id)}>
